a53a08537d8dc2abcf91b60c7b3bbe00d7857e7a
Emre Can <107356939+Can-Emre@users.noreply.github.com>
2023-02-05 17:42:03 +0300

    Add files via upload

---
+++ Patika.Dev_SQL_Homework6.sql
@@ -0,0 +1,14 @@
+-- 1. What is the average of the values in the rental_rate column in the film table?
+SELECT AVG(rental_rate) FROM film;
+
+-- 2. How many of the films in the film table start with the character 'C'?
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'C%';
+
+-- 3. Among the films in the film table, how many minutes is the longest (length) film with a rental_rate equal to 0.99?
+SELECT MAX(length) FROM film
+WHERE rental_rate = 0.99;
+
+-- 4. How many different replacement_cost values are there for the films longer than 150 minutes in the film table?
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 150;
